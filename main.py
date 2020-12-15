844215ac0f964b14e581353c2a118748bff48e56
Anshuman Ghosh <anshumanghosh95@gmail.com>
2020-12-15 20:05:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,8 +38,15 @@ if __name__ == '__main__':
 {
   "tree": {
     "nodes": [
-      {"id": "1", "left": "2", "right": null, "value": 1},
-      {"id": "2", "left": null, "right": null, "value": 2}
+      {"id": "1", "left": "2", "right": "3", "value": 1},
+      {"id": "2", "left": "4", "right": "5", "value": 2},
+      {"id": "3", "left": "6", "right": "7", "value": 3},
+      {"id": "4", "left": "8", "right": "9", "value": 4},
+      {"id": "5", "left": null, "right": null, "value": 5},
+      {"id": "6", "left": null, "right": null, "value": 6},
+      {"id": "7", "left": null, "right": null, "value": 7},
+      {"id": "8", "left": null, "right": null, "value": 8},
+      {"id": "9", "left": null, "right": null, "value": 9}
     ],
     "root": "1"
   }
